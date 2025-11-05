c116315c1348f83d77380813be9f554710c08dcb
ElifasPF <elifas.faustino@estudante.ifms.edu.br>
2025-11-05 17:27:24 -0400

    correção mensagem render

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -3,23 +3,21 @@ import 'dotenv/config'
 import cors from 'cors'
 import rotasAutenticadas from './rotas/rotas-autenticadas.js'
 import rotasNaoAutenticadas from './rotas/rotas-nao-autenticadas.js'
-import Auth from './middlewares/auth.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use(rotasNaoAutenticadas)
-app.use(Auth)
 app.use(rotasAutenticadas)
-
-app.listen(8000, () => {
-    console.log('Server is running on port 8000')
-})
 
 app.get('/', (req, res) => {
     res.status(200).json({ 
         mensagem: 'API TechShop is running!',
         status: 'OK'
     })
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000')
 })
